edd55ab56b65082ee238319e016b0205718903dd
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-10 20:44:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function breakOut(array, changeValue, stopValue) {
+
+  for (var i = 0; i < array.length; i++) {
+    array.push(array[i] = changeValue);
+    if(array[i] = stopValue); 
+  }
+  return array;
+}
