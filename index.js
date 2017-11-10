5390582d9cc8d5b0a98de79efcf18de3689adf3c
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-10 20:44:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function breakOut(array, changeValue, stopValue) {
 
   for (var i = 0; i < array.length; i++) {
     array.push(array[i] = changeValue);
-    if(array[i] = stopValue); 
+    if(array[i] = stopValue);
   }
   return array;
 }
